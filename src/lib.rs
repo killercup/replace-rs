90302ac58c7732e397deb671f28faa7fc2f35d38
Pascal Hertleif <killercup@gmail.com>
2018-05-04 22:29:01 +0200

    Simplify this whole thing
    Let's not make this too big a deal: It's for rustfix. We should always
    error when replacing an already replaced part of the data. Additionally,
    we also now track the original data and index into that, while otherwise
    using an `Rc<[u8]>` which allows cheaply build up new vectors of parts.

--- src/lib.rs
+++ src/lib.rs
@@ -3,151 +3,148 @@
 //! the same parts multiple times.
 
 #[deny(missing_docs)]
-
 #[macro_use]
 extern crate failure;
 #[cfg(test)]
 #[macro_use]
 extern crate proptest;
 
-use failure::Error;
-use std::ops::Range;
 use std::rc::Rc;
+use failure::Error;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum State {
-    Untouched,
-    Touched,
+    Initial,
+    Replaced(Rc<[u8]>),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Span {
-    state: State,
-    range: Range<usize>,
-    data: Rc<[u8]>,
+    /// Start of this span in parent data
+    start: usize,
+    /// up to end inculding
+    end: usize,
+    data: State,
 }
 
 /// A container that allows easily replacing chunks of its data
 #[derive(Debug, Clone, Default)]
 pub struct Data {
+    original: Vec<u8>,
     parts: Vec<Span>,
 }
 
 impl Data {
     /// Create a new data container from a slice of bytes
     pub fn new(data: &[u8]) -> Self {
-        if data.is_empty() {
-            return Data::default();
-        }
-
         Data {
+            original: data.into(),
             parts: vec![Span {
-                state: State::Untouched,
-                range: 0..data.len(),
-                data: data.into(),
+                data: State::Initial,
+                start: 0,
+                end: data.len(),
             }],
         }
     }
 
     /// Render this data as a vector of bytes
     pub fn to_vec(&self) -> Vec<u8> {
-        self.parts
-            .iter()
-            .map(|x| &x.data)
-            .fold(Vec::new(), |mut acc, d| {
-                acc.extend(d.iter());
-                acc
-            })
+        self.parts.iter().fold(Vec::new(), |mut acc, d| {
+            match d.data {
+                State::Initial => acc.extend_from_slice(&self.original[d.start..d.end]),
+                State::Replaced(ref d) => acc.extend_from_slice(&d),
+            };
+            acc
+        })
     }
 
     /// Replace a chunk of data with the given slice, erroring when this part
     /// was already changed previously.
-    pub fn replace_range_unless_touched(
-        &mut self,
-        range: Range<usize>,
-        data: &[u8],
-    ) -> Result<(), Error> {
-        self.replace_range(range, data, true)
-    }
-
-    /// Replace a chunk of data with a given slice and the option to return an
-    /// error if this part of the data was already changed earlier.
     pub fn replace_range(
         &mut self,
-        range: Range<usize>,
+        from: usize,
+        up_to_and_including: usize,
         data: &[u8],
-        error_if_touched: bool,
     ) -> Result<(), Error> {
-        if range.end == 0 {
-            return Ok(());
-        }
+        ensure!(
+            from <= up_to_and_including,
+            "Invalid range {}...{}, start is larger than end",
+            from,
+            up_to_and_including
+        );
+        ensure!(
+            up_to_and_including <= self.original.len(),
+            "Invalid range {}...{} given, original data is only {} byte long",
+            from,
+            up_to_and_including,
+            self.original.len()
+        );
 
+        // Since we error out when replacing an already replaced chunk of data,
+        // we can take some shortcuts here. For example, there can be no
+        // overlapping replacements -- we _always_ split a chunk of 'initial'
+        // data into three[^empty] parts, and there can't ever be two 'initial'
+        // parts touching.
+        //
+        // [^empty]: Leading and trailing ones might be empty if we replace
+        // the whole chunk. As an optimization and without loss of generality we
+        // don't add empty parts.
         let new_parts = {
-            use std::cmp::min;
-
-            let start = self.parts
+            let index_of_part_to_split = self.parts
                 .iter()
-                .position(|x| x.range.start <= range.start)
-                .ok_or_else(|| format_err!("No part found that contains range {:?}", range))?;
-            let end = self.parts.iter().rposition(|x| x.range.end >= range.end);
+                .position(|p| p.start <= from && p.end >= up_to_and_including)
+                .ok_or_else(|| {
+                    format_err!(
+                        "Could not find data slice that covers range {}..{}",
+                        from,
+                        up_to_and_including
+                    )
+                })?;
 
-            if error_if_touched {
-                let end = if let Some(end) = end {
-                    end + 1
-                } else {
-                    self.parts.len()
-                };
-                let any_touched = self.parts[start..end]
-                    .iter()
-                    .any(|p| p.state == State::Touched);
-                ensure!(
-                    !any_touched,
-                    "can't replace segments that were replaced previously"
-                );
+            let part_to_split = &self.parts[index_of_part_to_split];
+            ensure!(
+                part_to_split.data == State::Initial,
+                "Cannot replace slice of data that was already replaced"
+            );
+
+            let mut new_parts = Vec::with_capacity(self.parts.len() + 2);
+
+            // Previous parts
+            if let Some(ps) = self.parts.get(..index_of_part_to_split) {
+                new_parts.extend_from_slice(&ps);
             }
 
-            let mut res = Vec::with_capacity(self.parts.len());
-            if start > 0 {
-                res.extend(self.parts[..start.saturating_sub(1)].iter().cloned());
-            }
-
-            let start_part = &self.parts[start];
-
-            let start_range_end = range.start.saturating_sub(start_part.range.start);
-
-            if start_range_end > 0 {
-                let data = start_part.data[..min(start_range_end, start_part.data.len())].into();
-                res.push(Span {
-                    state: start_part.state,
-                    range: start_part.range.start..range.start,
-                    data,
+            // Keep initial data on left side of part
+            if from > part_to_split.start {
+                new_parts.push(Span {
+                    start: part_to_split.start,
+                    end: from,
+                    data: State::Initial,
                 });
             }
 
-            res.push(Span {
-                state: State::Touched,
-                range: range.start..range.end,
-                data: data.into(),
+            // New part
+            new_parts.push(Span {
+                start: from,
+                end: up_to_and_including,
+                data: State::Replaced(data.into()),
             });
 
-            if let Some(end) = end {
-                let end_part = &self.parts[end];
-                if !end_part.data.is_empty() {
-                    res.push(Span {
-                        state: end_part.state,
-                        range: range.end..end_part.range.end,
-                        data: end_part.data[min(
-                            range.end.saturating_sub(end_part.range.start),
-                            end_part.data.len().saturating_sub(1),
-                        )..]
-                            .into(),
-                    });
-
-                    res.extend(self.parts[end + 1..].iter().cloned());
-                }
+            // Keep initial data on right side of part
+            if up_to_and_including < part_to_split.end {
+                new_parts.push(Span {
+                    start: up_to_and_including + 1,
+                    end: part_to_split.end,
+                    data: State::Initial,
+                });
             }
 
-            res
+            // Following parts
+            if let Some(ps) = self.parts.get(index_of_part_to_split + 1..) {
+                new_parts.extend_from_slice(&ps);
+            }
+
+            new_parts
         };
 
         self.parts = new_parts;
@@ -168,44 +165,44 @@ mod tests {
     #[test]
     fn replace_some_stuff() {
         let mut d = Data::new(b"foo bar baz");
-
-        d.replace_range(4..6, b"lol", false).unwrap();
+        d.replace_range(4, 6, b"lol").unwrap();
         assert_eq!("foo lol baz", str(&d.to_vec()));
+    }
 
-        d.replace_range(4..6, b"lol", false).unwrap();
-        assert_eq!("foo lol baz", str(&d.to_vec()));
-
-        d.replace_range(4..6, b"foobar", false).unwrap();
-        assert_eq!("foo foobar baz", str(&d.to_vec()));
+    #[test]
+    fn replace_a_single_char() {
+        let mut d = Data::new(b"let y = true;");
+        d.replace_range(4, 4, b"mut y").unwrap();
+        assert_eq!("let mut y = true;", str(&d.to_vec()));
     }
 
     #[test]
     fn replace_multiple_lines() {
         let mut d = Data::new(b"lorem\nipsum\ndolor");
 
-        d.replace_range(6..11, b"lol", false).unwrap();
+        d.replace_range(6, 10, b"lol").unwrap();
         assert_eq!("lorem\nlol\ndolor", str(&d.to_vec()));
 
-        d.replace_range(12..18, b"lol", false).unwrap();
+        d.replace_range(12, 17, b"lol").unwrap();
         assert_eq!("lorem\nlol\nlol", str(&d.to_vec()));
     }
 
     #[test]
-    fn broken_replacements() {
-        let mut d = Data::new(b"foo");
+    #[should_panic(expected = "Cannot replace slice of data that was already replaced")]
+    fn replace_overlapping_stuff_errs() {
+        let mut d = Data::new(b"foo bar baz");
 
-        d.replace_range_unless_touched(4..7, b"lol").unwrap();
-        assert_eq!("foolol", str(&d.to_vec()));
+        d.replace_range(4, 6, b"lol").unwrap();
+        assert_eq!("foo lol baz", str(&d.to_vec()));
+
+        let x = d.replace_range(4, 6, b"lol").unwrap();
     }
 
     #[test]
-    fn dont_replace_twice() {
+    #[should_panic(expected = "original data is only 3 byte long")]
+    fn broken_replacements() {
         let mut d = Data::new(b"foo");
-
-        d.replace_range_unless_touched(4..7, b"lol").unwrap();
-        assert_eq!("foolol", str(&d.to_vec()));
-        println!("{:?}", d);
-        assert!(d.replace_range_unless_touched(4..7, b"lol").is_err());
+        d.replace_range(4, 7, b"lol").unwrap();
     }
 
     proptest! {
@@ -220,13 +217,13 @@ mod tests {
         fn replace_random_chunks(
             ref data in "\\PC*",
             ref replacements in prop::collection::vec(
-                (any::<Range<usize>>(), any::<Vec<u8>>()),
+                (any::<::std::ops::Range<usize>>(), any::<Vec<u8>>()),
                 1..1337,
             )
         ) {
             let mut d = Data::new(data.as_bytes());
-            for r in replacements {
-                let _ = d.replace_range(r.0.clone(), &r.1, false);
+            for &(ref range, ref bytes) in replacements {
+                let _ = d.replace_range(range.start, range.end, bytes);
             }
         }
     }
